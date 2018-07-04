37e9940645f5a3020a45fe6bc62a51aadee2aba0
Mohammad Zamani <maz.zamani@gmail.com>
2018-07-04 22:12:50 +0200

    argument corrected

--- main.py
+++ main.py
@@ -109,15 +109,15 @@ if __name__ == "__main__":
 
     #Dataset
     parser.add_argument('--identity', dest='identity', action='store_true', help='to regenerate what is given as in input sequence')
-    parser.add_argument('--rest', dest='rest', action='store_true',  help=' calculates the reminder of sum of a variable sequence to the given #class_no')
+    parser.add_argument('--mode', dest='mode', action='store_true',  help=' calculates the reminder of sum of a variable sequence to the given #class_no')
 
     parser.set_defaults(visualize=False)
     parser.set_defaults(reduced=False)
-    parser.set_defaults(rest=False)
+    parser.set_defaults(mode=False)
 
     args = parser.parse_args()
 
-    if args.rest:
+    if args.mode:
         MyDatatSet = RestDataSet
     else:
         MyDatatSet = IdentityDataset
